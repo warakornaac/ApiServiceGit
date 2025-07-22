a1e80466b269ca5253677c602d1dd585e844af8b
warakornaac <warakorn.pra@aac.co.th>
2025-07-22 08:15:38 +0700

    [controller] update function SearchItem project media portal

--- ApiService/Controllers/ItemsController.cs
+++ ApiService/Controllers/ItemsController.cs
@@ -23,66 +23,63 @@ namespace ApiService.Controllers
         }
 
         [HttpPost]
-        [Route("api/items")]
+        [Route("Items/SearchByDescription")]
+        [ApiKeyAuthorize]
         public IHttpActionResult PostItems([FromBody] List<ItemRequest> requests)
         {
-            if (requests == null)
-                return BadRequest("Invalid input");
-
+            if (requests == null) { 
+                return BadRequest("Empty data request");
+            }
             var responses = new List<ItemResponse>();
             var seenItemNos = new HashSet<string>();
-            // int counter = 1;
-
+            string errorMessageTxt = "success";
+            string[] keySearchArray = requests.Select(r => r.key).ToArray();
             var connectionString = ConfigurationManager.ConnectionStrings["MobileOrder_ConnectionString"].ConnectionString;
-            //string SQL = "select [CUSCOD],[CUSNAM] from [dbo].[v_CUSPROV] where CUSCOD = @cuscod";
             SqlConnection conn = new SqlConnection(connectionString);
-            conn.Open();
+            var okresp = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                ReasonPhrase = "Success"
+            };
             try
             {
-                foreach (var item in requests)
+                conn.Open();
+                // var keySearchArray = string.Join(" ", requests.Select(r => r.key));
+                string concatenatedKeys = string.Join(" ", keySearchArray);
+                SqlCommand cmd = new SqlCommand("P_Search_Item_Media", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@inSearch", concatenatedKeys);
+                SqlDataReader read = cmd.ExecuteReader();
+                while (read.Read())
                 {
-
-                    SqlCommand cmd = new SqlCommand("P_Search_Item_Media", conn);
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@inSearch", item.key);
-
-                    SqlDataReader read = cmd.ExecuteReader();
-                    while (read.Read())
+                    var itemNo = read["STKCOD"] != DBNull.Value ? read["STKCOD"].ToString() : "";
+                    var description = read["STKDES"] != DBNull.Value ? read["STKDES"].ToString() : "";
+                    if (!string.IsNullOrEmpty(itemNo) && seenItemNos.Add(itemNo))
                     {
-                        //responses.Add(new ItemResponse
-                        //{
-                        //    itemNo = read["STKCOD"] != DBNull.Value ? read["STKCOD"].ToString() : "",
-                        //    description = read["STKDES"] != DBNull.Value ? read["STKDES"].ToString() : ""
-                        //});
-
-                        var itemNo = read["STKCOD"] != DBNull.Value ? read["STKCOD"].ToString() : "";
-                        var description = read["STKDES"] != DBNull.Value ? read["STKDES"].ToString() : "";
-
-                        if (!string.IsNullOrEmpty(itemNo) && seenItemNos.Add(itemNo))
+                        responses.Add(new ItemResponse
                         {
-                            responses.Add(new ItemResponse
-                            {
-                                itemNo = itemNo,
-                                description = description
-                            });
-                        }
+                            itemNo = itemNo,
+                            fullDescription = description
+                        });
                     }
-                    //responses.Add(new ItemResponse
-                    //{
-                    //    itemNo = counter.ToString("D3"),
-                    //    description = $"Item {item.key} description"
-                    //});
-                    //counter++;
-
                 }
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                errorMessageTxt = ex.Message.ToString();
             }
+            DataRespond dataRes = new DataRespond();
+            dataRes.statusCode = Convert.ToInt32(okresp.StatusCode);
+            dataRes.errorMessage = errorMessageTxt;
+            dataRes.keySearch = string.Join(",  ", keySearchArray);
+            dataRes.result = responses;
 
+            String requestDataLog = JsonConvert.SerializeObject(requests);
+            string jsonReturn = JsonConvert.SerializeObject(dataRes);
 
-            return Ok(responses);
+            String lastId = _apiServerService.SaveApiResponse("Items/SearchByDescription", requestDataLog.ToString(), "");
+            _apiServerService.UpdateApiRespone(lastId, jsonReturn.ToString());
+
+            return Json(dataRes);
         }
 
         public class ItemRequest
@@ -92,9 +89,15 @@ namespace ApiService.Controllers
         public class ItemResponse
         {
             public string itemNo { get; set; }
-            public string description { get; set; }
+            public string fullDescription { get; set; }
         }
-
+        public class DataRespond
+        {
+            public int statusCode { get; set; }
+            public string errorMessage { get; set; }
+            public string keySearch { get; set; }
+            public List<ItemResponse> result { get; set; }
+        }
 
     }
 }
