7100f7f29e57770620aad5da0338f016ac6374c0
Kanyarat123 <66943981+Kanyarat123@users.noreply.github.com>
2026-08-25 14:50:26 +0700

    Claim

---
+++ ApiService/Controllers/ClaimUploadController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace ApiService.Controllers
+{
+    public class ClaimUploadController : ApiController
+    {
+        private readonly ApiServerController _apiServerService;
+
+        public ClaimUploadController()
+        {
+            _apiServerService = new ApiServerController();
+        }
+
+
+    }
+}
